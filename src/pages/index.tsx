d9d052eba2338519941aa5b05701fe78f2cab067
sinasafari <m.sinasafari@gmail.com>
2022-07-10 19:47:06 +0430

    feat: adding fonts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,51 +1,47 @@
-import {NextPageWithLayout} from "./_app";
-import {ReactElement} from "react";
-import {Center, Heading, Stack} from "@chakra-ui/react";
-import {BlankLayout} from "layouts";
-import Image from 'next/image';
-import Lottie from 'react-lottie';
-import animationData from '../../public/assets/lottie/donate.json';
+import { NextPageWithLayout } from "./_app";
+import { ReactElement } from "react";
+import { Center, Heading, Stack } from "@chakra-ui/react";
+import { BlankLayout } from "layouts";
+import Image from "next/image";
+import Lottie from "react-lottie";
+import animationData from "../../public/assets/lottie/donate.json";
 
 const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: animationData,
-    rendererSettings: {
-        preserveAspectRatio: "xMidYMid slice"
-    }
+  loop: true,
+  autoplay: true,
+  animationData: animationData,
+  rendererSettings: {
+    preserveAspectRatio: "xMidYMid slice",
+  },
 };
 const Home: NextPageWithLayout = () => {
-    return (
-        <div className='coming-soon'>
-            <div className='coming-soon-header'>
-                <div className='coming-soon-header__logo'>
-                    <Image src='/assets/images/icon.png'
-                           layout='fill'/>
-                </div>
-                <Heading as='h4' size='md'>قهوت با من</Heading>
-            </div>
-            <div className='coming-soon-content'>
-
-                <Stack spacing={6}>
-                    <Heading as='h4' size='lg'>
-                        حمایت طرفدارن خود را با چند کلیک داشته باشید😉
-                    </Heading>
-                    <Heading as='h4' size='md'>
-                        به زودی کنار شمایم . . .
-                    </Heading>
-                    <Lottie   options={defaultOptions}
-                              height={250}
-                              width={250}/>
-                </Stack>
-
-            </div>
-
+  return (
+    <div className="coming-soon">
+      <div className="coming-soon-header">
+        <div className="coming-soon-header__logo">
+          <Image src="/assets/images/icon.png" layout="fill" />
         </div>
-    );
+        <Heading as="h4" size="md">
+          قهوت با من
+        </Heading>
+      </div>
+      <div className="coming-soon-content">
+        <Stack spacing={6} px={5}>
+          <Heading as="h4" size="lg">
+            حمایت طرفدارن خود را با چند کلیک داشته باشید😉
+          </Heading>
+          <Heading as="h4" size="md">
+            به زودی کنار شمایم . . .
+          </Heading>
+          <Lottie options={defaultOptions} height={250} width={250} />
+        </Stack>
+      </div>
+    </div>
+  );
 };
 
 Home.getLayout = function getLayout(page: ReactElement) {
-    return <BlankLayout>{page}</BlankLayout>;
+  return <BlankLayout>{page}</BlankLayout>;
 };
 
 export default Home;
--- src/providers/themProvider/index.tsx
+++ src/providers/themProvider/index.tsx
@@ -1,13 +1,28 @@
 import React from "react";
-import {extendTheme, ChakraProvider} from "@chakra-ui/react";
+import { extendTheme, ChakraProvider } from "@chakra-ui/react";
+import { Global } from "@emotion/react";
 
 // TODO: add themeProvider color palette and font here
-const themeProvider = extendTheme();
+const themeProvider = extendTheme({
+  fonts: {
+    heading: `Dana, sans-serif`,
+    body: `Dana, sans-serif`,
+  },
+});
 
-export const ChakraUIThemeProvider: React.FC = ({children}) => {
-    return (
-        <ChakraProvider theme={themeProvider}>
-            {children}
-        </ChakraProvider>
-    );
+export const ChakraUIThemeProvider: React.FC = ({ children }) => {
+  return (
+    <ChakraProvider theme={themeProvider}>
+      <Global
+        styles={`
+            @font-face {
+                font-family: 'Dana';
+                font-style: normal;
+                src: url('/fonts/Dana-Regular.ttf') format('ttf');
+            }
+      `}
+      />
+      {children}
+    </ChakraProvider>
+  );
 };
